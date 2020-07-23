66091af6f007eb41f07a10fef0cf193733f4031f
karanvir <karanvir.singh@people10.com>
2020-07-23 23:37:34 +0530

    asfvrf

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
-console.log('console listening')
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
